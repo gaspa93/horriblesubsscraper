8b389ccb40009819ec01d4302d06e80899978f54
Mattia Gasparini <mattiagasparini2@gmail.com>
2020-03-21 18:14:33 +0100

    get torrent files to specific folder

--- HorribleSubsScraper.py
+++ HorribleSubsScraper.py
@@ -1,6 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 import webbrowser
+import re
+import time
+
+def getrows(parser):
+    return parser.find_all('tr', class_ = "success")
 
 quality = ["[480p]", "[720p]", "[1080p]"]
 
@@ -19,56 +24,42 @@ while True:
 
 #Add validation
 print("Now enter the range of episodes you want to download. \nFor example to download all episodes between and including episodes 1 and 10, enter 1 and then 10.\nIf you want to download the whole series, just press enter. You could then manually delete the torrents you don't want to keep from your client.\n")
-lrange = int(input("Enter the episode you want to start downloading from:\n"))
-urange = int(input("Enter the episode you want to stop downloading at:\n"))
+minepisode = int(input("Enter the episode you want to start downloading from:\n"))
+maxepisode = int(input("Enter the episode you want to stop downloading at:\n"))
 
-print("If nothing happens, the anime you entered isn't on HorribleSubs' torrents on nyaa.si. This will download the anime in the resolution you selected. If it doesn't find that resolution, it just doesn't download that episode so you'll have to do it manually.\n")
+# define number of pages to scrape
+n_pages = (maxepisode - minepisode)//25 + 1
 
-url = 'https://nyaa.si/user/HorribleSubs?f=0&c=0_0&q='+anime+'&o=asc&p='
+downloadurl = 'https://nyaa.si{}'
 
-def URLPage(p, url):
-    global html
-    url = url + str(p)
-    html = requests.get(url).content
+for p in range(1, n_pages+1):
 
-def amountOfPages():
-    page = 0
-    while True:
-        page += 1
-        URLPage(page, url)
-        try:
-            BeautifulSoup(html, features = "lxml").find_all(class_ = "success")[0].find_all(class_ = "text-center")[0].find_all("a", href = True)[1]['href']
-        
-        except IndexError:
-            return page - 1
+    url = 'https://nyaa.si/user/HorribleSubs?f=0&c=0_0&q='+anime+'&o=desc&p={}'.format(p)
+    html = requests.get(url).text
+    parser=BeautifulSoup(html, "html.parser")
 
-def amountOfRows(p):
-    row = 0
-    while True:
-        row += 1
-        URLPage(p, url)
-        try:
-            BeautifulSoup(html, features = "lxml").find_all(class_ = "success")[row].find_all(class_ = "text-center")[0].find_all("a", href = True)[1]['href']
-        
-        except IndexError:
-            return row
+    rows = getrows(parser)
 
-def downloader(episodename):
-    if quality[qselector] == episodename.split()[-1].strip(".mkv") and lrange <= float(episodename.split()[-2]) <= urange:            
-        print(episodename.strip(".mkv"))
-        webbrowser.open(BeautifulSoup(html, features = "lxml").find_all(class_ = "success")[row].find_all(class_ = "text-center")[0].find_all("a", href = True)[1]['href'])
-        
-    elif float(episodename.split()[-2]) > urange:
-        print("Completed!")
-        print(exit())
+    q = quality[qselector]
+    for r in rows:
 
-for page in range(1, amountOfPages()+1):
-    URLPage(page, url)
-    for row in range(0, amountOfRows(page)):
-        episodename = BeautifulSoup(html, features = "lxml").find_all(class_ = "success")[row].find_all("a")[1].get_text()
-        if "[HorribleSubs]" == episodename.split()[0]:
-            downloader(episodename)
+        # find title
+        elements = r.find_all('a')
+        for elem in elements:
+            if anime.upper() in elem['title'].upper():
+                title = elem['title']
+                break
 
-        else:
-            episodename = BeautifulSoup(html, features = "lxml").find_all(class_ = "success")[row].find_all("a")[2].get_text()
-            downloader(episodename)
+        # find quality
+        ep_number = int(re.search(r'[HorribleSubs].+-\s(\d+)', title).group(1))
+        if q in title and minepisode <= ep_number <= maxepisode:
+            endpoint = r.find_all('td', class_='text-center')[0].find_all('a')[0]['href']
+
+            print(title, downloadurl.format(endpoint))
+
+            torrent = requests.get(downloadurl.format(endpoint))
+
+            with open('downloaded/{}.torrent'.format(title), 'wb') as f:
+                f.write(torrent.content)
+
+            time.sleep(1)
