011fd5d526af9e58496f131ccf6bffd0bbab78f5
Mattia Gasparini <mattiagasparini2@gmail.com>
2020-03-22 18:59:37 +0100

    handle range of episodes that does not start from latest + better structure

--- HorribleSubsScraper.py
+++ HorribleSubsScraper.py
@@ -6,7 +6,9 @@ import time
 # template urls
 searchurl = 'https://nyaa.si/user/HorribleSubs?f=0&c=0_0&q={}&o=desc&p={}'
 downloadurl = 'https://nyaa.si{}'
+downloadfolder = 'downloaded' # set in your torrent client the automatic upload of torrents from this folder to completely automatizew the process
 quality = ["[480p]", "[720p]", "[1080p]"]
+order = ["asc", "desc"]
 
 # get user input
 anime = input("Enter anime name (as uploaded by HorribleSubs): ")
@@ -22,13 +24,14 @@ while True:
     except ValueError:
         print("Enter a valid number: 0, 1 or 2")
 
-#Add validation
-print("Now enter the range of episodes you want to download. \nFor example to download all episodes between and including episodes 1 and 10, enter 1 and then 10.\nIf you want to download the whole series, just press enter. You could then manually delete the torrents you don't want to keep from your client.\n")
+print("Now enter the range of episodes you want to download. \nFor example to download all episodes between and including episodes 1 and 10, enter 1 and then 10.")
 minepisode = int(input("Enter the episode you want to start downloading from:\n"))
 maxepisode = int(input("Enter the episode you want to stop downloading at:\n"))
 
 # define number of pages to scrape: web page contains 25 distinct episodes (x3 qualities)
-n_pages = (maxepisode - minepisode)//25 + 1
+# need latest episode to get correct number of pages
+latest_ep = int(re.search(r'[HorribleSubs].+-\s(\d+)', requests.get(searchurl.format(anime, '')).text).group(1))
+n_pages = (latest_ep - minepisode)//25 + 1
 
 # set quality to search
 q = quality[qselector]
@@ -41,28 +44,37 @@ for p in range(1, n_pages+1):
 
     rows = parser.find_all('tr', class_ = "success")
 
+    pag_episodes = {}
     for r in rows:
 
-        # find title element and check quality
-        elements = r.find_all('a')
-        eptitle = None
-        for elem in elements:
-            if anime.upper() in elem['title'].upper() and q in elem['title']:
+        episode = {}
+
+        # get list of elements that contain links
+        rowelements = r.find_all('a')
+        for elem in rowelements:
+            if elem.has_attr('title') and anime.upper() in elem['title'].upper():
                 eptitle = elem['title']
+                epnumber = int(re.search(r'[HorribleSubs].+-\s(\d+)', elem['title']).group(1))
+                episode['num'] = epnumber
                 break
 
-        if eptitle is not None:
+        endpoints = r.find_all('td', class_='text-center')[0].find_all('a')
 
-            ep_number = int(re.search(r'[HorribleSubs].+-\s(\d+)', title).group(1))
-            if minepisode <= ep_number <= maxepisode:
-                endpoint = r.find_all('td', class_='text-center')[0].find_all('a')[0]['href']
+        episode['download'] = endpoints[0]['href']
+        episode['magnet'] = endpoints[1]['href']
 
-                print(title, downloadurl.format(endpoint))
+        pag_episodes[eptitle] = episode
 
-                torrent = requests.get(downloadurl.format(endpoint))
-                with open('downloaded/{}.torrent'.format(title), 'wb') as f:
-                    f.write(torrent.content)
+    for eptitle in pag_episodes:
+        epnumber = pag_episodes[eptitle]['num']
+        endpoint = pag_episodes[eptitle]['download']
 
-                time.sleep(1)
-        else:
-            print('Quality {} for {} not found'.format(q, title))
+        if minepisode <= epnumber <= maxepisode and q in eptitle:
+
+            print(epnumber, downloadurl.format(endpoint))
+
+            torrent = requests.get(downloadurl.format(endpoint))
+            with open('{}/{}.torrent'.format(downloadfolder, eptitle), 'wb') as f:
+                f.write(torrent.content)
+
+            time.sleep(1)
